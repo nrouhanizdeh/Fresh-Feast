2a1d70e859c2fa0201bedfaa0114fa0cb8a1a9e2
Negin Rouhanizadeh <negin.rouhanizadeh@gmail.com>
2020-10-04 16:19:29 -0500

    Made changes required to get authentication working and alo add login page nav bar and style.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -15,14 +15,21 @@ module.exports = function(app) {
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login");
+
+  });
+
+  app.get("/signup", (req, res) => {
+    res.render("signup");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  // Add below lines for any new pages
   app.get("/members", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
